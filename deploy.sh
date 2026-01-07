4110ee827736e6de5954f8168af6e8d3891171c1
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2026-01-07 18:39:00 +0100

    trying access to pulseaudio

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,22 @@ echo "Starting container in interactive mode"
 lxc init ubuntu:24.04 $CONTAINER_NAME --config=user.user-data="$(cat app-config.yaml)"
 lxc list 
 lxc config device add $CONTAINER_NAME moj-kod disk source=$HOME/git/py-radio path=/mnt/app
+
+
+# 1. Pass the PulseAudio socket
+lxc config device add my-lxc-radio-automat-1 pulse-socket disk source=/run/user/1000/pulse/native path=/mnt/pulse-socket
+
+# 2. Pass the Pulse Cookie (Authentication)
+lxc config device add my-lxc-radio-automat-1 pulse-cookie disk source=$HOME/.config/pulse/cookie path=/mnt/pulse-cookie
+
+# 3. Add GPU/Audio device access (Standard for sound)
+lxc config device add my-lxc-radio-automat-1 snd unix-char path=/dev/snd
+
 lxc list 
 
 lxc config show  $CONTAINER_NAME
+
+echo "Starting container"
 
 lxc start $CONTAINER_NAME
 lxc list 
