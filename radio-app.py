00734e97e7496c2dcceec833da90c66f50825147
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2026-01-09 14:16:53 +0100

    more debug

--- radio-app.py
+++ radio-app.py
@@ -91,30 +91,31 @@ def get_radio_control_message_from_queue(config):
     sqs = boto3.resource('sqs')
 
     queue_name = config.get("radio.control.queue.name")
+    logger.debug(f"Queue name: {queue_name}")
 
     # Get the queue. This returns an SQS.Queue instance
     queue = sqs.get_queue_by_name(QueueName=queue_name)
 
     # You can now access identifiers and attributes
-    print(queue.url)
-    print("DelaySeconds", queue.attributes.get("DelaySeconds"))
-    print("queue attributes", str(queue.attributes))
+    logger.debug(queue.url)
+    logger.debug("DelaySeconds", queue.attributes.get("DelaySeconds"))
+    logger.debug("queue attributes", str(queue.attributes))
 
     # Process messages by printing out body and optional author name    
     message = None
     messages = queue.receive_messages()
     if len(messages) > 0:
-        print(f"There should be some message on queue: {queue_name}")
+        logger.debug(f"There should be some message on queue: {queue_name}")
         message = messages[0]
          # Print out the body 
-        print(f"Received message on {queue_name}: {message.body}")
+        logger.debug(f"Received message on {queue_name}: {message.body}")
 
         # Let the queue know that the message is processed
         message.delete()
         return message.body
         
     else:
-        print("No messages in queue")
+        logger.debug("No messages in queue")
         return None  
 
 if __name__ == "__main__":
